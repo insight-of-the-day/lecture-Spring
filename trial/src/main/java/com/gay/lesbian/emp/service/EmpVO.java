7a35d1cea594b4ce6bcb7fae662f997ac9e19d35
eiminfwe <activelistening@naver.com>
2023-07-10 13:39:10 +0900

    plz upload

---
+++ trial/src/main/java/com/gay/lesbian/emp/service/EmpVO.java
@@ -0,0 +1,17 @@
+package com.gay.lesbian.emp.service;
+
+import lombok.Data;
+
+@Data
+public class EmpVO {
+	String employee_id;
+	String first_name;
+	String last_name;
+	String email;
+	String hire_date;
+	String job_id;
+	String department_id;
+	String Salary;
+	
+	
+}
---
+++ trial/src/main/java/com/gay/lesbian/mapper/EmpMapper.java
@@ -0,0 +1,7 @@
+package com.gay.lesbian.mapper;
+
+import com.gay.lesbian.emp.service.EmpVO;
+
+public interface EmpMapper {
+	public EmpVO getEmp(EmpVO empVO);
+}
